119ef775e151c3b8ecb551633203a11fae170407
saikat <sguha@salesforce.com>
2020-10-16 14:42:26 +0530

    improve error message, set commands name and keyboard shortcuts properly to ctrl+z

--- vscode-plugin/out/extension.js
+++ vscode-plugin/out/extension.js
@@ -20,10 +20,10 @@ function activate(context) {
     // supply this to scripts as we are always importing freshly everytime so its easier for testing. so they can use this for any long term usages.
     // although most scripts shouldn't need it
     const store = {};
-    context.subscriptions.push(vscode.commands.registerCommand('extension.addToProps', () => __awaiter(this, void 0, void 0, function* () {
+    context.subscriptions.push(vscode.commands.registerCommand('progTemplates.execAndReplaceSelection', () => __awaiter(this, void 0, void 0, function* () {
         var _a;
         try {
-            console.log('props');
+            console.log('command invoked progTemplates.execAndReplaceSelection');
             const editor = vscode.window.activeTextEditor;
             if (editor) {
                 const document = editor.document;
@@ -34,7 +34,7 @@ function activate(context) {
                 const programmaticTemplatePath = configuration['ProgrammaticTemplatePath'];
                 // TODO: run basicAsyncExample.js maybe if this argument is not supplied
                 if (!programmaticTemplatePath) {
-                    vscode.window.showInformationMessage("Error: please set 'programmaticTemplatePath' to absolute path of the script you want to invoke, in your settings.json. If you want to try out an example, you can download any file , for ex: https://github.com/sktguha/programmaticTemplatesExamples/blob/master/basicAsyncExample.js and set it to the absolute path of the downloaded file ");
+                    vscode.window.showInformationMessage("Error: please set 'progTemplates.ProgrammaticTemplatePath' in your settings.json to absolute path of the script you want to invoke. If you want to try out an example, you can download any file , for ex: https://github.com/sktguha/programmaticTemplatesExamples/blob/master/basicAsyncExample.js and set it to the absolute path of the downloaded file ");
                     return;
                 }
                 const importFresh = require('import-fresh');
@@ -63,21 +63,14 @@ function activate(context) {
         }
         catch (err) {
             console.log("error occurred", err);
-            vscode.window.showInformationMessage("error occurred:" + err.toString() + " : JSON stringified error: " + JSON.stringify(err));
+            vscode.window.showInformationMessage("error occurred:" + err.toString() + " \nerror stack: " + err.stack + " : \nJSON stringified error: " + JSON.stringify(err));
         }
     })));
-    context.subscriptions.push(vscode.commands.registerCommand('extension.addToState', () => {
-        // const axios = require('axios');
-        // const editor = vscode.window.activeTextEditor;
-        // const lastSel = editor?.selections.sort((a, b) => a.start.line - b.start.line).pop();
-        // let cursorPosition = lastSel?.end;
-        // let wordRange = cursorPosition && editor?.document.getWordRangeAtPosition(cursorPosition);
-        // let highlight = wordRange && editor?.document.getText(wordRange);
-        // axios.get('http://localhost:9090?command=state&path='+vscode.window.activeTextEditor?.document.fileName+"&line="+lastSel?.end.line+"&char="+lastSel?.end.character+"&high="+highlight);
-    }));
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
-function deactivate() { }
+function deactivate() {
+    vscode.window.showInformationMessage("Sorry to see you go!. Please mail me sktguha2@gmail.com or create new issue at https://github.com/sktguha/programmaticTemplates/issues/new");
+}
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- vscode-plugin/src/extension.ts
+++ vscode-plugin/src/extension.ts
@@ -11,9 +11,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// although most scripts shouldn't need it
 	const store = {};
 	context.subscriptions.push(
-		vscode.commands.registerCommand('extension.addToProps', async () => {
+		vscode.commands.registerCommand('progTemplates.execAndReplaceSelection', async () => {
 			try {
-				console.log('props');
+				console.log('command invoked progTemplates.execAndReplaceSelection');
 				const editor = vscode.window.activeTextEditor;
 				if (editor) {
 					const document = editor.document;
@@ -25,7 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
 					const programmaticTemplatePath = configuration['ProgrammaticTemplatePath'];
 					// TODO: run basicAsyncExample.js maybe if this argument is not supplied
 					if (!programmaticTemplatePath) {
-						vscode.window.showInformationMessage("Error: please set 'programmaticTemplatePath' to absolute path of the script you want to invoke, in your settings.json. If you want to try out an example, you can download any file , for ex: https://github.com/sktguha/programmaticTemplatesExamples/blob/master/basicAsyncExample.js and set it to the absolute path of the downloaded file ");
+						vscode.window.showInformationMessage("Error: please set 'progTemplates.ProgrammaticTemplatePath' in your settings.json to absolute path of the script you want to invoke. If you want to try out an example, you can download any file , for ex: https://github.com/sktguha/programmaticTemplatesExamples/blob/master/basicAsyncExample.js and set it to the absolute path of the downloaded file ");
 						return;
 					}
 					const importFresh = require('import-fresh');
@@ -53,22 +53,13 @@ export function activate(context: vscode.ExtensionContext) {
 				}
 			} catch (err) {
 				console.log("error occurred", err);
-				vscode.window.showInformationMessage("error occurred:" + err.toString() + " : JSON stringified error: " + JSON.stringify(err));
+				vscode.window.showInformationMessage("error occurred:" + err.toString() + " \nerror stack: " + err.stack + " : \nJSON stringified error: " + JSON.stringify(err));
 			}
-		})
-	);
-	context.subscriptions.push(
-		vscode.commands.registerCommand('extension.addToState', () => {
-			// const axios = require('axios');
-			// const editor = vscode.window.activeTextEditor;
-			// const lastSel = editor?.selections.sort((a, b) => a.start.line - b.start.line).pop();
-			// let cursorPosition = lastSel?.end;
-			// let wordRange = cursorPosition && editor?.document.getWordRangeAtPosition(cursorPosition);
-			// let highlight = wordRange && editor?.document.getText(wordRange);
-			// axios.get('http://localhost:9090?command=state&path='+vscode.window.activeTextEditor?.document.fileName+"&line="+lastSel?.end.line+"&char="+lastSel?.end.character+"&high="+highlight);
 		})
 	);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {
+	vscode.window.showInformationMessage("Sorry to see you go!. Please mail me sktguha2@gmail.com or create new issue at https://github.com/sktguha/programmaticTemplates/issues/new");
+}
